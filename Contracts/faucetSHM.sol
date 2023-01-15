f6b92eb7c8d52ad21a47b7cb4baba4b7aba6aaf8
Rishu009 <87556810+Rishu009@users.noreply.github.com>
2023-01-15 12:10:18 +0530

    Update faucetSHM.sol

--- Contracts/faucetSHM.sol
+++ Contracts/faucetSHM.sol
@@ -14,6 +14,6 @@ contract faucetSHM {
         if(block.timestamp < userPreviousWithdrawTime[msg.sender] + 43200) {revert cooldown();} 
         if(address(this).balance < 50 ether) {revert faucetNotFunded();}
         userPreviousWithdrawTime[msg.sender] = block.timestamp; //Current faucet user address records current UNIX time for cooldown check. 
-        payable(msg.sender).transfer(50 ether);             //Send 20 LINK to current faucet user address.
+        payable(msg.sender).transfer(50 ether);             //Send 20 LINK to current faucet user address.0x05b7fBE24C98b81C558Aa2fC1e490f7E83AA62Bc
     }
 }
